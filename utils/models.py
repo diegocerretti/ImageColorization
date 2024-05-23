c2d957a3bd893ec4927d02376fc1971b5c748824
sandromikautadze <sandromikautadze@yahoo.com>
2024-05-24 00:58:13 +0200

    add sigmoid output to cnn

--- utils/models.py
+++ utils/models.py
@@ -76,6 +76,7 @@ class CNN(nn.Module):
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(9, 64)
         self.fc2 = nn.Linear(64, 2 * self.height * self.width)  # adjust accordingly
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor):
         """
@@ -91,6 +92,7 @@ class CNN(nn.Module):
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.sigmoid(x)
         x = x.view(-1, 2, self.height, self.width)
         return x
 
