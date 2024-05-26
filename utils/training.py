805df8eb2e453caeafac09b5217b3baf66c296a0
sandromikautadze <sandromikautadze@yahoo.com>
2024-05-26 22:57:45 +0200

    change # checkpoints to 4 from 3 in gan

--- utils/training.py
+++ utils/training.py
@@ -234,8 +234,8 @@ def train_gan(epochs: int, discriminator: torch.nn.Module, generator: torch.nn.M
             loop.set_postfix(d_loss=epoch_d_loss / (loop.n + 1), g_loss=epoch_g_loss / (loop.n + 1))            
 
         if save_checkpoints and (epochs > 10):
-            if epoch in [int(epochs / 3) - 1, 2 * int(epochs / 3) - 1, epochs - 1]:
-                # save three checkpoints
+            if epoch in [int(epochs / 4) - 1, 2 * int(epochs / 4) - 1, 3 * int(epochs / 4) - 1, epochs - 1]:
+                # save four checkpoints
                 save_model(generator, f"{file_name}_{epoch + 1}")
 
         d_losses.append(epoch_d_loss / len(train_loader))
